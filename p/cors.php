d08b9e478a154ac13cb51aaad8183081edcf5b2e
laundy <dev@josolutions.de>
2026-04-01 20:28:14 +0200

    Add configuration and headers for CORS

---
+++ p/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+// Set CORS headers with every response
+$config = include('../config.php');
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (in_array($origin, $config->allowedOrigins, true)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    exit;
+}
